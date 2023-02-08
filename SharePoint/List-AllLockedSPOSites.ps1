d956e3b4b372c2b86b06f4b1c44513d47b086f92
Peter Schmidt <peter@msdigest.net>
2023-02-08 10:04:19 +0100

    Update List-AllLockedSPOSites.ps1

--- SharePoint/List-AllLockedSPOSites.ps1
+++ SharePoint/List-AllLockedSPOSites.ps1
@@ -1,3 +1,17 @@
+<#
+.SYNOPSIS
+  List all SharePoint Online sites and show Locked state
+.DESCRIPTION
+  List all SharePoint Online sites and show Locked state
+  Tested with SharePoint Online
+.OUTPUTS
+  Exports all SharePoint site lock state to defined CSV file
+.NOTES
+  Author:  Peter Schmidt (https://msdigest.net)
+.VERSION
+  Version: 1.0  2023.02.07  Initial version
+#>
+
 #Variables for Admin Center and Site Collection URL
 $AdminCenterURL = "https://contoso-admin.sharepoint.com"
 
